ecfb1002f7da432ad35281b20df10f0bf5df2c4c
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-18 11:10:05 +0300

    Update 01_task_except.py

--- Module7/practice/exceptions/01_task_except.py
+++ Module7/practice/exceptions/01_task_except.py
@@ -4,3 +4,24 @@
 # На вход программе подается строка формата nxm (x - латинская буква икс).
 # Пример входных данных: 12x6
 # Если данные вводятся в неверном формате, сообщить об этом и запросить ввод заново.
+f = True
+while f:
+    string = input("print data in format MxN to get count of M-Squares")
+    try:
+        if string.find('x') < 0:
+            raise ImportError("В строке должен быть х")
+        n, m = map(int, string.split('x'))
+
+    except ValueError as e:
+        print("M и N должны быть целыми числами")
+    except ImportError as e:
+        print(e)
+    except Exception as e:
+        print(e.__class__)
+        print(e)
+    else:
+        count = n // m
+        f = False
+
+print(count)
+
